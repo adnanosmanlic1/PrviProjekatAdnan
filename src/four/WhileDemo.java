4ad8c2163e5eb626d8521da6190860eb3261622c
adnan.osmanlic1 <adnan.osmanlic@outlook.com>
2021-11-14 17:08:24 +0100

    Četvrti susret i drugi commit

---
+++ src/four/WhileDemo.java
@@ -0,0 +1,16 @@
+package four;
+
+/** PETLJA - blok koda koji se izvršava sve dok je zadovoljen uslov petlje
+/**  while(condition) {blok koda}
+*/
+public class WhileDemo {
+    public static void main(String[] args) {
+        int number = 1; // 1. početni uslovi definirni kroz vrijablu petlje
+        while(number<= 10){  // 2. uslov petlje izveden kroz varijablu petlje
+            System.out.println(number);
+            number++;//3. korak petlje izveden kroz varijablu petlje
+
+        }
+
+    }
+}
