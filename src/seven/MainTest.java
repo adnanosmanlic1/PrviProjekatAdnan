c91815da657949b3a004005b6b4fa7a8600e70f8
adnan.osmanlic1 <adnan.osmanlic@outlook.com>
2021-11-27 19:59:29 +0100

    Objektno orijentisano programiranje

--- src/seven/MainTest.java
+++ src/seven/MainTest.java
@@ -1,14 +1,11 @@
 package seven;
 
+import seven.human.Employe;
 import seven.human.Person;
+import seven.human.Professor;
 
 public class MainTest {
     public static void main(String[] args) {
-        Person person = new Person();
-        Gender gender1 =Gender.MALE;
-        Gender gender2 = Gender.FEMALE;
 
-        System.out.println(gender1.hashCode());
-        System.out.println(gender2.hashCode());
     }
 }
--- src/seven/human/Person.java
+++ src/seven/human/Person.java
@@ -1,11 +1,70 @@
 package seven.human;
 
 import seven.Gender;
+import seven.animal.Dog;
 
 public class Person {
     private int id;
-    private  String name;
-    private String surname;
-    private Gender gender;
+    private String name;  // polje
+    private String surname; //polje
+    private Gender gender; // polje
+    private int age;
+    private Dog dog;
 
+    public Person(){
+        super();
+        System.out.println("Person...");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
 }
+
+
+
+
